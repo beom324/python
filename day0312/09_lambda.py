8e8285f367916b66e404be73303a398072bd688f
beom324 <scarlet2010@nate.com>
2024-03-12 14:51:58 +0900

    lambda practice

---
+++ day0312/09_lambda.py
@@ -0,0 +1,17 @@
+data =[]
+add = lambda x:data.append(x+1)
+
+# def add(x):
+#     data.append(x+1)
+arr=[1,2,3]
+
+for a in arr:
+    add(a)
+
+
+for a in data:
+    # 2 3 4
+    print(a,end=" ")
+
+
+
---
+++ day0312/10_lambda.py
@@ -0,0 +1,9 @@
+
+add = lambda x,y:x+y
+# def add2(x,y) :
+#     return x+y;
+
+
+#5
+print(add(2,3))
+
