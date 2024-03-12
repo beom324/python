fc489538963cbcbf912c3115e2fb0306e7f184fc
beom324 <scarlet2010@nate.com>
2024-03-12 12:02:52 +0900

    spring flask_cors

--- day0312/server.py
+++ day0312/server.py
@@ -2,8 +2,10 @@ from flask import request
 from flask import render_template
 from flask import Flask, url_for
 from iris import getIris
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/iris",methods=['get'])
 def irisForm():
@@ -18,6 +20,16 @@ def irisSubmit():
     result = getIris(sepal_length,sepal_width,petal_length,petal_width)
 
     return render_template("/iris.html",results=result)
+
+@app.route("/getIris",methods=['post'])
+def irisSubmitAjax():
+    sepal_length = float(request.form["sepal_length"])
+    sepal_width = float(request.form["sepal_width"])
+    petal_length = float(request.form["petal_length"])
+    petal_width = float(request.form["petal_width"])
+    result = getIris(sepal_length,sepal_width,petal_length,petal_width)
+
+    return result
 
 @app.route("/")
 def index():
