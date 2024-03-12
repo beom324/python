d15ffed2fb48f304848295370e5af1ea746e7924
beom324 <scarlet2010@nate.com>
2024-03-12 15:39:28 +0900

    python method

---
+++ day0312/11_eval.py
@@ -0,0 +1,4 @@
+data=[10,20,30,40]
+cmd = "print(data)";
+#[10, 20, 30, 40]
+eval(cmd)
---
+++ day0312/13_filter.py
@@ -0,0 +1,8 @@
+
+data=[1,2,3,4,5,6,7,8,9,10]
+result = filter(lambda x:x%2==0,data)
+#[2, 4, 6, 8, 10]
+# print(list(result))
+list(reversed(data))
+#[10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+print(list(reversed(data)))
